c09cc2a30959c09fbf80a4d694c16f6d192f2941
Frank <1305679132@qq.com>
2019-05-23 15:18:30 +0800

    add many

--- changejson.php
+++ changejson.php
@@ -2,7 +2,7 @@
 	$total = $_POST['total'];
 	$total = htmlspecialchars($total);
 	$total = str_replace("'","\"",$total);
-	$myfile = fopen("./fjson/test.json","w") or die("Unable to open file!");
+	$myfile = fopen("./fjson/222.json","w") or die("Unable to open file!");
 	fwrite($myfile, $total);
 	fclose($myfile);
 	echo "完成";
---
+++ denglu.handle.php
@@ -0,0 +1,17 @@
+<?php
+	require_once('connect.php');
+	$user = htmlspecialchars($_POST[user]);
+	$user = str_replace("'","\'",$user);
+	$password = htmlspecialchars($_POST[password]);
+	$password = str_replace("'","\'",$password);
+	$sql = "select * from que_user where name = '$user' and psd = '$password'";
+	$query = mysql_query($sql);
+	$result = mysql_num_rows($query);
+	if($result >= 1){
+		setcookie("user","frank");
+		echo "<script>window.location.href='uindex.php'</script>";
+	}
+	else {
+		echo "<script>alert('账号或者密码错误');window.location.href='index.html'</script>";
+	}
+?>
