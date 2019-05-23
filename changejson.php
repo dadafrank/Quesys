4ba90d2fbce1b3798afe15a6270575fc5eddf185
Frank <1305679132@qq.com>
2019-05-23 11:12:55 +0800

    add many

--- changejson.php
+++ changejson.php
@@ -1,5 +1,6 @@
 <?php
 	$total = $_POST['total'];
+	$total = htmlspecialchars($total);
 	$total = str_replace("'","\"",$total);
 	$myfile = fopen("./fjson/test.json","w") or die("Unable to open file!");
 	fwrite($myfile, $total);
