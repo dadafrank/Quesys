27964db95978fa93a51ab621c7a8065bc6c21070
Frank <1305679132@qq.com>
2019-05-29 14:35:18 +0800

    the wrong that many poeple update

--- ansup.php
+++ ansup.php
@@ -5,7 +5,36 @@
 	$queid = str_replace("<","&lt;",$queid);
 	$queid = str_replace(">","&gt;",$queid);
 	$queid = str_replace("\"","&quot;",$queid);
-	
+	$b = $_POST[b];
+	$number = "";
+	// 获取数据
+	$fileget = file_get_contents("./fjson/$queid.json") or die("what?");
+	$datas = json_decode($fileget,true);
+	$bbb = (int)$datas["number"];
+	// 判断两个b是否一样,不一样就让他们想等,只能减少出错率,不能百分比避免.
+	if($b != $bbb) {
+		$b = $bbb;
+		$b++;
+		// 为了保持json数据一致
+		if($b < 10) {
+			$number = "000" . $b;
+		}
+		else if($b < 100){
+			$number = "00" . $b;
+		}
+		else if($b < 1000){
+			$number = "0" . $b;
+		}
+		else {
+			$number = $b;
+		}
+	}
+	// 优先修改number,防止错乱
+	$file = fopen("./fjson/$queid.json","r+");
+	fseek($file,12);
+	fwrite($file,$number);
+	fseek($file,0);
+	// 继续修改内容
 	$total = $_POST[total];
 	// $total = htmlspecialchars($total);
 	$total = str_replace("<","&lt;",$total);
@@ -13,13 +42,8 @@
 	$total = str_replace("\"",",&quot;",$total);
 	$total = str_replace("**","\"",$total);
 	$total = str_replace("\n","<br/>",$total);
-	$number = $_POST[number];
-	$b = $_POST[b];
-	// 判断是否有锁
-	$file = fopen("./fjson/$queid.json","r+");
-	fseek($file,12);
-	fwrite($file,$number);
-	fseek($file,0);
+	
+	
 	if($b != 1) {
 		$total = "," . $total;
 	}
